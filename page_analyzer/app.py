ce93e3bc24bb7958aa3e63f4c1f3ccd3fcd957df
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-03-26 12:46:29 +0400

    feat: integrate dotenv for environment variable management and update dependencies

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+load_dotenv()
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
 @app.route("/")
