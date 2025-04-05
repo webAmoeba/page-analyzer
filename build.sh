e38e891fe31c87a52f90844a8288d64011bdfa11
Valera Galeev <71322091+webAmoeba@users.noreply.github.com>
2025-04-05 08:18:51 +0400

    feat: start making db

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-# скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+# Postgres позволяет подключиться к удаленной базе указав ссылку на нее после флага -d
+# ссылка подгрузится из переменной окружения, которую нам нужно будет указать на сервисе деплоя
+# дальше мы загружаем в поключенную базу наш sql-файл с таблицами
+make install && psql -a -d $DATABASE_URL -f database.sql
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
+import psycopg2
+
 
 app = Flask(__name__)
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
 
 
 @app.route("/")
